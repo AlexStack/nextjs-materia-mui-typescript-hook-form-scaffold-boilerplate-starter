e15a0c73a4489ce8bcba5c6451cfd8bd30065c26
Alex Zeng <alex@10168.com>
2023-09-24 11:18:41 +1300

    Initial commit from Create Next App

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+import clsx, { ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/** Merge classes with tailwind-merge with clsx full feature */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
