8c51fb64c21554d0e5e0b8cdd97feb0adfcdd1ee
Alex Zeng <alex@10168.com>
2024-07-21 12:08:17 +1200

    feat: add timeout to getApiResponse

--- src/utils/shared/get-api-response.ts
+++ src/utils/shared/get-api-response.ts
@@ -1,21 +1,40 @@
 import { IS_PROD } from '@/constants';
 import { consoleLog } from '@/utils/shared/console-log';
 
+/**
+ * Makes an API request and returns the response data.
+ *
+ * @param apiEndpoint - The API endpoint URL.
+ * @param requestData - The request data to be sent in the request body.
+ * @param method - The HTTP method for the request (default: 'GET').
+ * @param revalidate - The time in seconds to cache the data (default: 3600 seconds in production, 120 seconds otherwise).
+ * @param headers - The headers to be included in the request.
+ * @param timeout - The timeout in milliseconds for the request (default: 100000 = 100 seconds).
+ * @returns The response data from the API.
+ * @throws An error if the API request fails or times out.
+ */
 export const getApiResponse = async <T>({
   apiEndpoint,
   requestData,
   method = 'GET',
   revalidate = IS_PROD ? 3600 : 120, // cache data in seconds
   headers,
+  timeout = 100000, // 100 seconds
 }: {
   apiEndpoint: string;
   requestData?: BodyInit;
   method?: 'POST' | 'GET' | 'PUT' | 'DELETE';
   revalidate?: number;
   headers?: HeadersInit;
+  timeout?: number;
 }) => {
   try {
     const startTime = Date.now();
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const timeoutId = setTimeout(() => controller.abort(), timeout);
+
     const response = await fetch(apiEndpoint, {
       method,
       body: requestData,
@@ -23,6 +42,7 @@ export const getApiResponse = async <T>({
       next: {
         revalidate,
       },
+      signal,
     });
     if (!response.ok) {
       consoleLog('🚀 Debug getApiResponse requestData:', requestData);
@@ -38,9 +58,18 @@ export const getApiResponse = async <T>({
         duration > 2000 ? '💔' : '-'
       } ${apiEndpoint}`
     );
-
+    clearTimeout(timeoutId);
+    // if is not valid JSON, return response
+    if (!response.headers.get('content-type')?.includes('application/json')) {
+      return response as T;
+    }
     return (await response.json()) as T;
   } catch (error) {
+    if (error instanceof Error && error.name === 'AbortError') {
+      throw new Error(
+        'Fetch request timed out: ' + (timeout / 1000).toFixed(1) + ' s'
+      );
+    }
     consoleLog('getApiResponse error:', error);
   }
 
